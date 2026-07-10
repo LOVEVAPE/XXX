ab4fae0e0b86da329e47a9f31dd8b77a8aee9d0c
LOVEVAPE <an4522634@gmail.com>
2026-07-10 22:44:07 +0900

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -98,8 +98,8 @@ local function downloadFile(path, func)
   if not isfile(path) and not getgenv().LunarVapeDeveloper then
     local suc, res = pcall(function()
       return game:HttpGet(
-        ('https://raw.githubusercontent.com/AtTheZenith/LunarVape/'
-          .. (isfile 'Lunar Vape/Profiles/Commit.txt' and readfile 'Lunar Vape/Profiles/Commit.txt' or 'main')
+        ('https://raw.githubusercontent.com/LOVEVAPE/LunarVape/'
+          .. (isfile 'Lunar Vape/Profiles/Commit.txt' and readfile 'Lunar Vape/Profiles/Commit.txt' or 'master')
           .. '/'
           .. (string.gsub(path, 'Lunar Vape/', ''))):gsub(' ', '%%20'),
         true
@@ -161,7 +161,7 @@ end
 
 if not getgenv().LunarVapeDeveloper then
   local _, subbed = pcall(function()
-    return game:HttpGet 'https://github.com/AtTheZenith/LunarVape'
+    return game:HttpGet 'https://github.com/LOVEVAPE/LunarVape'
   end)
   local commit = subbed:find 'currentOid'
   commit = commit and subbed:sub(commit + 13, commit + 52) or nil
